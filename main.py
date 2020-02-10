3d1e89cc3fcd91a5d14686b4819f8352d3718991
ConnorT6 <snjp.9702@gmail.com>
2020-02-10 19:38:29 +0530

    opencv python bilinear interpolation algorithm

--- main.py
+++ main.py
@@ -1,63 +1,63 @@
 import cv2 as cv
 import numpy as np
 
-im = cv.imread("samples/image.png", cv.IMREAD_REDUCED_GRAYSCALE_2)
+im = cv.imread("samples/image.png", cv.IMREAD_GRAYSCALE)
 scale = 4
-originalRows = int(im.shape[0])
-originalCols = int(im.shape[1])
-rows = int(scale * originalRows) - (scale - 1)
-cols = int(scale * originalCols) - (scale - 1)
+numOfRowsOnOriginal = int(im.shape[0])
+numOfColsOnOriginal = int(im.shape[1])
+numOfRowsOnZoomed = int(scale * numOfRowsOnOriginal) - (scale - 1)
+numOfColsOnZoomed = int(scale * numOfColsOnOriginal) - (scale - 1)
 
-zoomed = np.zeros((rows, cols), dtype=im.dtype)
+zoomed = np.zeros((numOfRowsOnZoomed, numOfColsOnZoomed), dtype=im.dtype)
 
-for i in range(0, rows):
-    for j in range(0, cols):
+for i in range(0, numOfRowsOnZoomed):
+    for j in range(0, numOfColsOnZoomed):
         color1 = None
         color2 = None
         color3 = None
         color4 = None
-        tmpRow = int(i / scale)
-        tmpCol = int(j / scale)
+        rowOnOriginal = int(i / scale)
+        colOnOriginal = int(j / scale)
 
-        if tmpRow != (originalRows - 1) and tmpCol != (originalCols - 1):
-            rowIndex = tmpRow * scale
-            rowIndex1 = (tmpRow + 1) * scale
+        if rowOnOriginal != (numOfRowsOnOriginal - 1) and colOnOriginal != (numOfColsOnOriginal - 1):
+            rowIndex = rowOnOriginal * scale
+            rowIndex1 = (rowOnOriginal + 1) * scale
 
-            colIndex = tmpCol * scale
-            colIndex1 = (tmpCol + 1) * scale
+            colIndex = colOnOriginal * scale
+            colIndex1 = (colOnOriginal + 1) * scale
 
             verticalColor1 = None
             verticalColor2 = None
 
             if i == rowIndex and j == colIndex:
-                verticalColor1 = im[tmpRow, tmpCol]
+                verticalColor1 = im[rowOnOriginal, colOnOriginal]
                 verticalColor2 = -1
             else:
-                if i == colIndex:
-                    color1 = im[tmpRow, tmpCol]
-                    color3 = im[tmpRow + 1, tmpCol]
-                    verticalDif1 = i - tmpRow * scale
-                    verticalDif2 = (tmpRow + 1) * scale - i
+                if j == colIndex: #if the pixel we are processing is on a major column. we don't need 2 vertical color values
+                    color1 = im[rowOnOriginal, colOnOriginal]
+                    color3 = im[rowOnOriginal + 1, colOnOriginal]
+                    verticalDif1 = i - rowOnOriginal * scale
+                    verticalDif2 = (rowOnOriginal + 1) * scale - i
                     verticalColor1 = ((color1 * verticalDif2) / (verticalDif1 + verticalDif2)) + (
                             (color3 * verticalDif1) / (verticalDif1 + verticalDif2))
                     verticalColor2 = -1
                 else:
-                    color1 = im[tmpRow, tmpCol]
-                    color2 = im[tmpRow, tmpCol + 1]
-                    color3 = im[tmpRow + 1, tmpCol]
-                    color4 = im[tmpRow + 1, tmpCol + 1]
-                    verticalDif1 = i - tmpRow * scale
-                    verticalDif2 = (tmpRow + 1) * scale - i
+                    color1 = im[rowOnOriginal, colOnOriginal]
+                    color2 = im[rowOnOriginal, colOnOriginal + 1]
+                    color3 = im[rowOnOriginal + 1, colOnOriginal]
+                    color4 = im[rowOnOriginal + 1, colOnOriginal + 1]
+                    verticalDif1 = i - rowOnOriginal * scale
+                    verticalDif2 = (rowOnOriginal + 1) * scale - i
                     verticalColor1 = ((color1 * verticalDif2) / (verticalDif1 + verticalDif2)) + (
                             (color3 * verticalDif1) / (verticalDif1 + verticalDif2))
                     verticalColor2 = ((color2 * verticalDif2) / (verticalDif1 + verticalDif2)) + (
                             (color4 * verticalDif1) / (verticalDif1 + verticalDif2))
 
-            if verticalColor2 == -1:
+            if verticalColor2 == -1:#if the pixel is on a intersection of a row and colmn
                 zoomed[i, j] = verticalColor1
             else:
-                horizontalDif1 = j - tmpCol * scale
-                horizontalDif2 = (tmpCol + 1) * scale - j
+                horizontalDif1 = j - colOnOriginal * scale
+                horizontalDif2 = (colOnOriginal + 1) * scale - j
                 tempColor = ((verticalColor1 * horizontalDif2) / (horizontalDif1 + horizontalDif2)) + (
                         (verticalColor2 * horizontalDif1) / (horizontalDif1 + horizontalDif2))
                 zoomed[i, j] = tempColor
